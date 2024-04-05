11419be3bdb5aa9f4d72e2cb77d38a5fa51cedc3
ibrahimdev23 <ibtheonemohamed13@gmail.com>
2024-04-04 21:28:02 -0500

    f

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     manifest: true,
     rollupOptions: {
       // overwrite default .html entry
-      input: '/path/to/main.js',
+      input: 'client/src/main.jsx',
     },
   }
 })
