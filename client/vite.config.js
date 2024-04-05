233aaa67d1b81fd533942002e15b105cd17e22c4
ibrahimdev23 <ibtheonemohamed13@gmail.com>
2024-04-04 21:40:13 -0500

    de

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,14 +3,25 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   build: {
-    // generate .vite/manifest.json in outDir
-    manifest: true,
-    rollupOptions: {
-      // overwrite default .html entry
-      input: 'client/src/main.jsx',
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, 'client/src/main.jsx'),
+      name: 'MyLib',
+      // the proper extensions will be added
+      fileName: 'main.js',
     },
-  }
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
 })
-
